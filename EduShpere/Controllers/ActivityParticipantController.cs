31de337c9d766f05dafc9766ac0a302c9ab61e68
HongPhuc03 <topluaq@gmail.com>
2025-09-10 18:25:40 +0700

    ESP58_Register-Participate-in-Activities

---
+++ EduShpere/Controllers/ActivityParticipantController.cs
@@ -0,0 +1,28 @@
+﻿using EduShpere.Application;
+using EduShpere.Application.DTOs;
+using EduShpere.Application.Services;
+using EduShpere.Shared.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduShpere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityParticipantController : ControllerBase
+    {
+        private readonly IActivityParticipantService _service;
+        public ActivityParticipantController(IActivityParticipantService service)
+        {
+            _service = service;
+        }
+        [HttpPost(ApiEndpoints.ActivityParticipant.AddActivityParticipant)]
+        public async Task<IActionResult> AddActivityParticipant([FromBody] AddParticipantDto dto)
+        {
+            if (dto == null)
+                return BadRequest(new ResponseDto<string>(null, ErrorMessages.Generic.UnknownError, 400));
+            var result = await _service.AddActivityParticipant(dto);
+            return Ok(new ResponseDto<ActivityParticipantResponseDto>(result, "Thêm người tham gia hoạt động thành công",200));
+        }
+    }
+}
--- EduShpere/Program.cs
+++ EduShpere/Program.cs
@@ -1,7 +1,9 @@
 using System.Text;
 using EduShpere.Application;
 using EduShpere.Application.Mappings;
+using EduShpere.Application.Services;
 using EduShpere.Infrastructure;
+using EduShpere.Infrastructure.Repositories;
 using EduShpere.Infrastructure.Repositories.OneTimeLogin;
 using KidNet;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -24,6 +26,7 @@ namespace EduShpere
 
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IOneTimeLoginRepository, OneTimeLoginRepository>();
+            builder.Services.AddScoped<IActivityParticipantRepository, ActivityParticipantRepository>();
 
             // Add Configs
             builder.Services.Configure<GoogleAuthConfig>(builder.Configuration.GetSection("GoogleOAuth"));
@@ -34,9 +37,10 @@ namespace EduShpere
             builder.Services.AddScoped<IHttpContextService,HttpContextService>();
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
-
+            builder.Services.AddScoped<IActivityParticipantService, ActivityParticipantService>();
             builder.Services.AddHttpContextAccessor();
             builder.Services.AddAutoMapper(typeof(UserProfile).Assembly);
+            builder.Services.AddAutoMapper(typeof(ActivityParticipantProfile).Assembly);
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
