a703aceb7c4afe52999dc72bb3587672f779515a
CaptainMarvelouss <phanduonghuy2003@gmail.com>
2025-09-12 18:54:17 +0700

    ESP42_ESP49 CRU and Paging Accounts

---
+++ Application/DTOs/AuthDto/UpdateUserDto.cs
@@ -0,0 +1,36 @@
+﻿using EduShpere.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduShpere.Application.DTOs.AuthDto
+{
+    public class UpdateUserDto
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string? Username { get; set; } = null!;
+
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string? Email { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? FirstName { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? LastName { get; set; } = null!;
+
+
+        [MaxLength(15)]
+        public string? PhoneNumber { get; set; } = null!;
+        public DateTime? Birthdate { get; set; } = null!;
+        public string? Address { get; set; } = null!;
+        public string? AvatarUrl { get; set; } = null!;
+
+        public UserRole? Role { get; set; } = null!;
+    }
+}
