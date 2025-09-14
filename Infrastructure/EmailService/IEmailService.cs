74728d93d01fe76b87490c4a0c865b37f07cad3d
HongPhuc03 <topluaq@gmail.com>
2025-09-15 04:26:24 +0700

    fix config

--- Infrastructure/EmailService/IEmailService.cs
+++ Infrastructure/EmailService/IEmailService.cs
@@ -5,5 +5,4 @@ public interface IEmailService
 {
     void SendTestEmail(User user);
     Task SendEmailAsync(string toEmail, string subject, string body, bool isHtml = true);
-    Task SendEmailInvitaion(string userFullName, string userEmail);
 }
