94204d24489a8b1a27b12442fe5176d93ab0dabd
HongPhuc03 <topluaq@gmail.com>
2025-12-14 15:40:35 +0700

    moderation

---
+++ Application/DTOs/ModerationDto/ClassGroupDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduShpere.Application.DTOs
+{
+    public class ClassGroupModerationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Grade { get; set; }
+        public string Class => $"{Grade}{Name}";
+    }
+}
